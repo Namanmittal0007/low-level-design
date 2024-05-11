3db197dc9f9cefef6c09b13fc9dafab9e46a876f
naman-mittal_ghub <Naman.Mittal@blueyonder.com>
2024-05-11 20:08:35 +0530

    initial comit - project setup

---
+++ Observer-Design-Pattern/src/test/java/com/naman/observerdesignpattern/ObserverDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naman.observerdesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObserverDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
